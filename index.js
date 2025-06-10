aa3382eb17513a137a22043d310c31aad284f873
CasTwitch <Casplant@gmail.com>
2025-06-10 16:45:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ document.getElementById("increaseBtn").onclick = function(){
 }
 
  ComfyJS.onCommand = ( user, command, message, extra ) => {
-        if( command === "test" ) 
+        if( command === "test" ) {
     count+=1;
-
+    document.getElementById("countLabel").innerHTML = count()
+          console.log( "!test JS was typed in chat" );
         }
     }
         

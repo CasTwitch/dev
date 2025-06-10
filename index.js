c67c9f84b8139d96107397c2451baab0ef900178
CasTwitch <Casplant@gmail.com>
2025-06-10 20:06:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,41 @@ let count2 = 0;
 let count3 = 0;
 let count4 = 0;
 
-ComfyJS.onCommand = (user, command, message, extra) => {
+function updateBars() {
+  const total = count1 + count2 + count3 + count4 || 1; // prevent divide by 0
+
+  document.getElementById("bar1").style.width = (count1 / total) * 100 + "%";
+  document.getElementById("label1").textContent = `Test1: ${count1}`;
+
+  document.getElementById("bar2").style.width = (count2 / total) * 100 + "%";
+  document.getElementById("label2").textContent = `Test2: ${count2}`;
+
+  document.getElementById("bar3").style.width = (count3 / total) * 100 + "%";
+  document.getElementById("label3").textContent = `Test3: ${count3}`;
+
+  document.getElementById("bar4").style.width = (count4 / total) * 100 + "%";
+  document.getElementById("label4").textContent = `Test4: ${count4}`;
+}
+
+ComfyJS.onCommand = (user, command) => {
   switch (command) {
     case "test1":
       count1++;
-      document.getElementById("count1").innerHTML = count1;
-      console.log("!test1 typed by", user);
       break;
     case "test2":
       count2++;
-      document.getElementById("count2").innerHTML = count2;
-      console.log("!test2 typed by", user);
       break;
     case "test3":
       count3++;
-      document.getElementById("count3").innerHTML = count3;
-      console.log("!test3 typed by", user);
       break;
     case "test4":
       count4++;
-      document.getElementById("count4").innerHTML = count4;
-      console.log("!test4 typed by", user);
       break;
+    default:
+      return;
   }
+  updateBars();
 };
 
 ComfyJS.Init("casthekingofawesomeness", null, ["Castheking02", "Djzandr", "casthekingofawesomeness"]);
+

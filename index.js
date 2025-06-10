2e077443894488d10377e6d06cb7ce09fda08121
CasTwitch <Casplant@gmail.com>
2025-06-10 15:16:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let count = 0;
 
-document.getElementById("decreaseBtn").onclick = function(){
-    count-=1;
-    document.getElementById("countLabel").innerHTML = count;
-}
+
 
 document.getElementById("resetBtn").onclick = function(){
     count=0;

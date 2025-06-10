7d6d988821d928df792ef567d1b3e6eb2f421043
CasTwitch <Casplant@gmail.com>
2025-06-10 15:47:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ document.getElementById("increaseBtn").onclick = function(){
         if( command === "test" ) {
     count+=1;
     document.getElementById("countLabel").innerHTML = count;
-}
-            }
+
           console.log( "!test COUNTING was typed in chat" );
-        
+        }
+            }
       
-     ComfyJS.Init( "casthekingofawesomeness", null, [ "Castheking02", "Djzandr", "casthekingofawesomeness" ] );
+     ComfyJS.Init( "casthekingofawesomeness", null, [ "Castheking02", "Djzandr", "option4" ] );
 
 
 

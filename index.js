19703a8910cd8174378fe2f63fc7bb12f99a28df
CasTwitch <Casplant@gmail.com>
2025-06-10 16:24:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ document.getElementById("increaseBtn").onclick = function(){
         if( command === "test" ) {
     count+=1;
     document.getElementById("countLabel").innerHTML = count()
+
+             ComfyJS.onCommand = ( user, command, message, extra ) => {
+        if( command === "reset" ) {
+    count=0;
+    document.getElementById("countLabel").innerHTML = count()
         }
     }
         

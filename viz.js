03bd80cad25a8aa80fdca2d0dcfa6058de5a8647
CasTwitch <Casplant@gmail.com>
2025-06-11 14:16:44 +0200

    Update viz.js

--- viz.js
+++ viz.js
@@ -1,6 +1,6 @@
 async function fetchVotes() {
   try {
-    const response = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhM7953Iy_jZU5i2DP7nzeBw_K-zmvLg7VtoYWE80m6YfmKUN2NIMRm86ocPtRk9Veh5vN9aik8bbQY41petpIYaQIieF9xX4IDCyrsCyLNeicnK_9agz5sxt2njnfdDZg7k75aX10LkGJGzX_Ee69_g2dTmE7mnu5zhjJeL7tsKfis42gR8O5HKS5RQv9dEr3Xb_pWTMBQuW_CjQ9mmPk_GraJJhcPpKKAIaGlFBi75Sm2rX_oHklf0VRRnnSuCKOL8RCZEX3uJz0QAjuUKjKqVfs591oi9RfeJpI5&lib=MeaYsQTwvRmx50QmXqr06QZq1e8rbxTfw'); // Replace with your doGet URL
+    const response = await fetch('https://script.google.com/macros/s/AKfycbx9VVRbCFTNTcLL8KMSr9WHxOQD8eav9onyTbo_ryzSSwBY3coX1-X7tgI0PB3e_H4uFQ/exec'); // Replace with your doGet URL
     const data = await response.json();
 
     // Example: data might be {votered: 3, voteblue: 5, votegreen: 2, voteyellow: 4}
